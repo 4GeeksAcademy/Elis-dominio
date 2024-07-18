04b9a581c9a05d82595c11f42c9f592c3bbf1fa2
ElisG24 <160186837+ElisG24@users.noreply.github.com>
2024-07-18 14:53:40 +0000

    ejercicio realaizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["mis", "los", "nuestros"];
+  let abjetiv = ["grandes", "ultimos", "modernos"];
+  let noun = ["almacenes", "perros", "lapices"];
+  let tlds = [".org", ".de", ".dog"];
+
+  let domainNames = [];
+  for (let pr of pronoun) {
+    for (let abj of abjetiv) {
+      for (let n of noun) {
+        for (let tl of tlds) {
+          // domainNames.push(pr + abj + n + tl);
+          console.log(pr + abj + n + tl);
+        }
+      }
+    }
+  }
+  // console.log(domainNames);
 };
